4abdee1082480f5dd75d84d3793e4d5e384cd66d
jasoncustodio <jmaxcustodio@gmail.com>
2020-03-15 20:21:19 -0700

    Files needed for tableview

---
+++ TopAlbums/Controller/AlbumTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AlbumTableViewController.swift
+//  TopAlbums
+//
+//  Created by Jason Maxwell Custodio on 3/15/20.
+//  Copyright © 2020 Jason Maxwell Custodio. All rights reserved.
+//
+
+import Foundation
---
+++ TopAlbums/Utility/NetworkRequest.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkRequest.swift
+//  TopAlbums
+//
+//  Created by Jason Maxwell Custodio on 3/15/20.
+//  Copyright © 2020 Jason Maxwell Custodio. All rights reserved.
+//
+
+import Foundation
