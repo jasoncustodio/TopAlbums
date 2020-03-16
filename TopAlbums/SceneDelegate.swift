bfa49508a84152e7359008ee569ee4a3b3a4f5b5
jasoncustodio <jmaxcustodio@gmail.com>
2020-03-15 20:22:57 -0700

    change root view

--- TopAlbums/SceneDelegate.swift
+++ TopAlbums/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: AlbumTableViewController())
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
